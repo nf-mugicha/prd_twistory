b6990a65dd11cdf7131bcccf990e474a553d131b
nf-mugicha <natsuki.fujimoto.kk@gmail.com>
2020-01-07 16:24:14 +0900

    set

--- backend/app/TweetPost.py
+++ backend/app/TweetPost.py
@@ -40,8 +40,10 @@ class TweetPost(object):
         generated_text = self.generated_text
         # params = {'status': generated_text +
         #           str('\n #ついじぇね\n') + str(self.display_name) + " ボットがツイートしました" + str('\n https://aitter-twigene.me')}
+        # params = {'status': generated_text +
+        #           str('\n\n #ついじぇね #') + str(self.display_name) + "ボットのツイート" + "\n自分っぽいツイートを自動生成してみよう！" + str('\n https://aitter-twigene.me')}
         params = {'status': generated_text +
-                  str('\n\n #ついじぇね #') + str(self.display_name) + "ボットのツイート" + "\n自分っぽいツイートを自動生成してみよう！" + str('\n https://aitter-twigene.me')}
+                  str('\n\n #ついじぇね') + "\n自分っぽいツイートを自動生成してみよう！" + str('\n https://aitter-twigene.me')}
         req = twitter_oath.post(url, params)
 
         if req.status_code == 200:
