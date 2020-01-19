9f287de7d35dfbb10428b16559b7f69c3e74c790
nf-mugicha <natsuki.fujimoto.kk@gmail.com>
2020-01-19 18:13:57 +0900

    bug fix

--- backend/app/functions/get_3200_user_timeline.py
+++ backend/app/functions/get_3200_user_timeline.py
@@ -51,7 +51,8 @@ def get_3200_user_timeline(account, user_timeline_3200_raw, logger, filepath):
                 screen_name=account,
                 count=200
             )
-        return 
+            if len(latest_tweets) > 0:
+                break
     # 最新のツイートid取得
     latest_id = latest_tweets[0].id
     logger.info("now's latest id: {}".format(str(latest_tweets[0].id)))
